4507533fb2cc732b203dfbfa34271196c374b026
Talha Khalil <Talha.khalil707@gmail.com>
2024-10-08 14:40:54 +0500

    Commit

--- Home/models.py
+++ Home/models.py
@@ -14,7 +14,7 @@ class feedback(models.Model):
     
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = models.ImageField(upload_to='profile_pics/', default='default.jpg')
+    profile_picture = models.ImageField(upload_to='profile_pics/')
 
     def __str__(self):
         return f'{self.user.username} Profile'
