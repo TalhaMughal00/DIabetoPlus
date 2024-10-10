f65d459dbfed978d5fe89b47bd3535580838c9d7
Talha Khalil <Talha.khalil707@gmail.com>
2024-10-10 13:22:33 +0500

    Commit

--- DiabetoPlus/settings.py
+++ DiabetoPlus/settings.py
@@ -138,3 +138,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# Email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'diabetoplus@gmail.com'
+EMAIL_HOST_PASSWORD = 'swcz bxez gbce abei'
--- DiabetoPlus/urls.py
+++ DiabetoPlus/urls.py
@@ -22,6 +22,7 @@ from Glucose_Record import views as Glucose_Record_views
 from recommendations import views as recommendations_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -57,4 +58,9 @@ urlpatterns = [
     path('profile/', home_views.profile, name="profile"),
     # Update Profile Picture
     path('profile/picture/', home_views.profile_pic, name='profile_pic'),
+    # Reset Password
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
