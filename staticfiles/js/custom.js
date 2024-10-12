e2cce6a2d19f41d83924ec64aced1e48837fc0ed
Talha Khalil <Talha.khalil707@gmail.com>
2024-10-12 21:13:37 +0500

    Commit

--- staticfiles/js/custom.js
+++ staticfiles/js/custom.js
@@ -29,3 +29,13 @@ function calculateBMI() {
 
     document.getElementById('result').innerText = `Your BMI is ${bmiRounded}`;
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    $('.dropdown-submenu > a').on("click", function(e) {
+      var submenu = $(this).next('ul');
+      $('.dropdown-submenu .dropdown-menu').not(submenu).hide();
+      submenu.toggle();
+      e.stopPropagation();
+      e.preventDefault();
+    });
+  });
