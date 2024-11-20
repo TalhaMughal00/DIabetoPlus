203e513a6012f2f1c7b044a31c4a563d930877f1
Talha Khalil <Talha.khalil707@gmail.com>
2024-11-20 21:10:20 +0500

    commit

--- DiabetoPlus/settings.py
+++ DiabetoPlus/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3w(y(radzer83=gvnacr4%^h12x_k8m^422db&=3r7^eqw&)i=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.100.7', '127.0.0.1', 'localhost']
 
 
 # Application definition
